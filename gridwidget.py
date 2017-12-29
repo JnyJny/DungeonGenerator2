62d3b7fe9da25eae6a8dc98262ae920aa305819b
Erik O'Shaughnessy <erik.oshaughnessy@gmail.com>
2017-12-28 22:14:57 -0600

    added background property and bugs to GridWidget

--- gridwidget.py
+++ gridwidget.py
@@ -7,14 +7,14 @@ from kivy.graphics import Line, Rectangle, Color
 class GridWidget(Widget):
     '''
     '''
-    grid_unit = NumericProperty(0)
+    grid_unit = NumericProperty(11)
     foreground = ListProperty([1,1,1,1])
     background = ListProperty([0,0,0,1])
 
     vlines = ListProperty([])
     hlines = ListProperty([])
     
-    def __init__(self, grid_unit=5,
+    def __init__(self, grid_unit=11,
                  foreground=None,
                  background=None,
                  **kwargs):
@@ -22,16 +22,23 @@ class GridWidget(Widget):
         self.grid_unit = grid_unit
         self.foreground = foreground or self.foreground
         self.background = background or self.background
+        self.bg = Color(self.background)
         self.fg = Color(self.foreground)
+        self.canvas.before.add(self.bg)
         self.canvas.add(self.fg)
         self.bind(size=self._resize)
         self.bind(grid_unit=self._resize)
         self.bind(pos=self._move)
         self.bind(foreground=self._foreground)
+        self.bind(background=self._background)
 
     def _foreground(self, instance, value):
 
         self.fg.rgba = self.foreground
+
+    def _background(self, instance, value):
+        
+        self.bg.rgba = self.background
 
     def _resize(self, instance, value):
         '''
@@ -44,11 +51,11 @@ class GridWidget(Widget):
 
         vdim = (self.width // self.grid_unit) + 1
         hdim = (self.height // self.grid_unit) + 1
-        vsz = (1, self.height)
-        hsz = (self.width, 1)
+        vsz = (1, self.height+1)
+        hsz = (self.width+1, 1)
             
-        self.vlines = [Rectangle(size=vsz) for _ in range(vdim)]
-        self.hlines = [Rectangle(size=hsz) for _ in range(hdim)]
+        self.vlines = [Rectangle(size=vsz) for _ in range(int(vdim))]
+        self.hlines = [Rectangle(size=hsz) for _ in range(int(hdim))]
 
         for l in self.vlines:
             self.canvas.add(l) 
